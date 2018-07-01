79b4b00827e0f792c5ef4e8afc22039cc3c14e35
openscript <1105080+openscript@users.noreply.github.com>
2018-07-01 11:04:19 +0200

    Add webpack and typescript configuration

---
+++ webpack.ts
@@ -0,0 +1,62 @@
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as path from 'path';
+import {Configuration, DefinePlugin} from 'webpack';
+
+const config: Configuration = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+
+    devtool: 'source-map',
+    devServer: {
+        historyApiFallback: true
+    },
+
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json'],
+        modules: [path.resolve(__dirname, 'examples'), 'node_modules']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: '/node_modules/',
+                use: [{
+                    loader: 'awesome-typescript-loader',
+                    options: {
+                        configFileName: 'tsconfig.json'
+                    }
+                }]
+            }, {
+                test: /\.scss$/,
+                exclude: '/node_modules/',
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'sass-loader',
+                    query: {
+                        sourceMap: true
+                    }
+                }]
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'public'
+        }]),
+        new DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('development'),
+                BASE_URL: JSON.stringify('/')
+            }
+        })
+    ]
+};
+
+export = config;
