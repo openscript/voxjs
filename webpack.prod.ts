e84887ff9fdfc6eac5cffa8c1b942ca0169b3fdf
openscript <1105080+openscript@users.noreply.github.com>
2018-07-01 11:38:43 +0200

    Add TravisCI configuration

---
+++ webpack.prod.ts
@@ -0,0 +1,76 @@
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as path from 'path';
+import * as UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import {Configuration, DefinePlugin, LoaderOptionsPlugin} from 'webpack';
+
+const config: Configuration = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'build')
+    },
+
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json'],
+        modules: [path.resolve(__dirname, 'examples'), 'node_modules']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: '/node_modules/',
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
+                }, {
+                    loader: 'awesome-typescript-loader',
+                    options: {
+                        configFileName: 'tsconfig.prod.json'
+                    }
+                }]
+            }, {
+                test: /\.scss$/,
+                exclude: '/node_modules/',
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'sass-loader',
+                    query: {
+                        sourceMap: false
+                    }
+                }]
+            }
+        ]
+    },
+
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            })
+        ]
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'public'
+        }]),
+        new LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+                BASE_URL: JSON.stringify('/')
+            }
+        })
+    ]
+};
+
+export = config;
