aa0013205639ea0d197b318204f570b792396ae4
openscript <1105080+openscript@users.noreply.github.com>
2018-07-02 13:27:00 +0200

    Add firebase configuration

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAcNVQA50f7T7v0bIKcmX1jcOIrKLHnprA',
+    authDomain: 'vox-js.firebaseapp.com',
+    databaseURL: 'https://vox-js.firebaseio.com',
+    projectId: 'vox-js',
+    storageBucket: 'vox-js.appspot.com',
+    messagingSenderId: '641738977479'
+};
