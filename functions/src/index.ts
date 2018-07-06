aea550642f9483a6f6ad791ee9fe2a9cda804271
openscript <1105080+openscript@users.noreply.github.com>
2018-07-06 22:40:27 +0200

    Fix on reply creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-export const onReplyCreation = functions.database.ref('/replys').onCreate((snapshot, context) => {
+export const onReplyCreation = functions.database.ref('/replys/{pushId}').onCreate((snapshot, context) => {
     return snapshot.ref.child('created_at').set(admin.database.ServerValue.TIMESTAMP);
 });
