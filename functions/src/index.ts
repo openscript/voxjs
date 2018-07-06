0505e1f4656bdf7216d8c98b6cf339d045906496
openscript <1105080+openscript@users.noreply.github.com>
2018-07-06 22:32:10 +0200

    Add timestamp on reply creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+export const onReplyCreation = functions.database.ref('/replys').onCreate((snapshot, context) => {
+    return snapshot.ref.child('created_at').set(admin.database.ServerValue.TIMESTAMP);
 });
