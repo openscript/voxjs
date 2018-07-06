fab3f3603b1a1733e61eb409a5f67b2c257dcbbc
openscript <1105080+openscript@users.noreply.github.com>
2018-07-06 23:38:59 +0200

    Set username on reply creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,5 +2,10 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 export const onReplyCreation = functions.database.ref('/replys/{pushId}').onCreate((snapshot, context) => {
-    return snapshot.ref.child('created_at').set(admin.database.ServerValue.TIMESTAMP);
+    if (context.auth) {
+        const setCreatedAt = snapshot.ref.child('created_at').set(admin.database.ServerValue.TIMESTAMP);
+        const setUserId = snapshot.ref.child('uid').set(context.auth.uid);
+        const setAuthor = admin.auth().getUser(context.auth.uid).then((user) => snapshot.ref.child('author').set(user.displayName));
+        return Promise.all([setCreatedAt, setUserId, setAuthor]);
+    }
 });
