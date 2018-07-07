58bd6a522a56c4a0574095e0a4708c4462f4796f
openscript <1105080+openscript@users.noreply.github.com>
2018-07-07 22:09:28 +0200

    Enhance minimal theme

--- src/components/replyDisplay.tsx
+++ src/components/replyDisplay.tsx
@@ -8,13 +8,22 @@ interface Props {
 
 export class ReplyDisplay extends Component<Props> {
     public render() {
+        const authorPhoto = this.props.reply.authorPhotoURL
+            ? <img class='author-photo' src={this.props.reply.authorPhotoURL} />
+            : <div class='author-photo default' />;
+
         return (
-            <li>
-                <div class='meta' id={this.props.reply.key}>
-                    <span class='author'>{this.props.reply.author}</span>
-                    <span class='datetime'>{dayjs(this.props.reply.createdAt).toString()}</span>
+            <li id={this.props.reply.key}>
+                <div class='header'>
+                    {authorPhoto}
+                    <div class='meta' >
+                        <span class='author'>{this.props.reply.author}</span>
+                        <span class='datetime'>{dayjs(this.props.reply.createdAt).toString()}</span>
+                    </div>
                 </div>
-                {this.props.reply.message}
+                <div class='message'>
+                    {this.props.reply.message}
+                </div>
             </li>
         );
     }
