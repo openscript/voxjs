9155dbc2cae55f82ab80871f9c007024fdbce614
openscript <1105080+openscript@users.noreply.github.com>
2018-07-08 10:42:11 +0200

    Split display name and use first part

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@ export const onReplyCreation = functions.database.ref('/replies/{pathId}/{pushId
         const setCreatedAt = snapshot.ref.child('createdAt').set(admin.database.ServerValue.TIMESTAMP);
         const setUserId = snapshot.ref.child('uid').set(context.auth.uid);
         const setUserInfo = admin.auth().getUser(context.auth.uid).then((user) => {
-            const setAuthor = snapshot.ref.child('author').set(user.displayName);
+            const setAuthor = snapshot.ref.child('author').set(user.displayName.split(' ')[0]);
             const setAuthorPhoto = snapshot.ref.child('authorPhotoURL').set(user.photoURL);
             return Promise.all([setAuthor, setAuthorPhoto]);
         });
